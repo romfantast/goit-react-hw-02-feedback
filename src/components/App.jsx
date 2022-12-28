13640826be015a420baeb93f9dc7e8652a78bf2a
romfantast <romfantast@gmail.com>
2022-12-28 13:13:09 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
-import ContactsApp from './ContactsTask/ContactsApp/ContactsApp';
 import FeedbackApp from './FeedbackTask/FeedbackApp/FeedbackApp';
 
 export const App = () => {
   return (
     <>
       <FeedbackApp />
-      <ContactsApp />
     </>
   );
 };
